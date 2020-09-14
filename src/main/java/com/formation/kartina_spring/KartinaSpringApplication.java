2fd452b2cf6d0f64637f93c21e77bf9a06f00ea9
Erick <erickfrancodelgado@hotmail.com>
2020-09-14 11:16:31 +0200

    add datasource.properties with datasource username and password but not in git

--- src/main/java/com/formation/kartina_spring/KartinaSpringApplication.java
+++ src/main/java/com/formation/kartina_spring/KartinaSpringApplication.java
@@ -2,8 +2,14 @@ package com.formation.kartina_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource(value = "classpath:application.properties"),
+        @PropertySource(value = "classpath:datasource.properties", ignoreResourceNotFound = true)
+})
 public class KartinaSpringApplication {
 
     public static void main(String[] args) {
