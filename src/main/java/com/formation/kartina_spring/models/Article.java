31cace52955ccf419dd15f1c4ef1f77c8ee9cd2c
Marc-Antoine DUPIRE <marc-antoinedupire@hotmail.fr>
2020-09-14 16:18:00 +0200

    implementation class Article

--- src/main/java/com/formation/kartina_spring/models/Article.java
+++ src/main/java/com/formation/kartina_spring/models/Article.java
@@ -1,4 +1,30 @@
 package com.formation.kartina_spring.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Calendar;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long articleRef;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false, length = 10)
+    private Float prixBase;
+
+    @Temporal(TemporalType.DATE)
+    private Calendar dateAjout;
 }
