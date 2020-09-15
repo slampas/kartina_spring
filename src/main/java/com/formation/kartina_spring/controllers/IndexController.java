9fafa61cd2514ca67c14a2e63704360c44035191
Marc-Antoine DUPIRE <marc-antoinedupire@hotmail.fr>
2020-09-15 16:19:42 +0200

    Templates des footer et header Fragment index pour page principale

---
+++ src/main/java/com/formation/kartina_spring/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.formation.kartina_spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("fragment", "index");
+        return "index";
+    }
+}
