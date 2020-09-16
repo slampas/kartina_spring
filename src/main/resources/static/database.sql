fcc71cb7b59ea701ce6c8840e75ca66c6714aa4b
Baziz <amir@baziz.fr>
2020-09-16 22:38:56 +0200

    Add (articles + artistes)

--- src/main/resources/static/database.sql
+++ src/main/resources/static/database.sql
@@ -1,3 +1,74 @@
-/*Fichier à completer */insert into Adresse 
+/*Fichier à completer */
+
+/*Les adresses*/
+
+insert into  adresse (num,voie,complement_voie,ville,code_postal, pays,nom_adresse) 
+values ("01 ","rue ","complement de voix","Lille",59000,"France","le nom d'adresse"),
+("08 ","impasse "," voix","Londre",44000,"UK","ADRESSE angleterre, le nom d'adresse"),
+("03 ","rue ","complement de voix","Madrid",569000,"Espagne","adresse le nom d'adresse"),
+("08 ","impasse "," voix","Londre",44000,"UK","ADRESSE angleterre, le nom d'adresse"),
+("02 ","rue ","complement de voix","Milan",559000,"Italie","adresse le nom d'adresse"),
+("08 ","impasse "," voix","Londre",44000,"UK","ADRESSE angleterre, le nom d'adresse"),
+("03 ","rue ","complement de voix","Madrid",569000,"Espagne","adresse le nom d'adresse"),
+("03 ","rue ","complement de voix","Madrid",569000,"Espagne","adresse le nom d'adresse"),
+("08 ","impasse "," voix","Londre",44000,"UK","ADRESSE angleterre, le nom d'adresse"),
+("04 ","avenue ","complement de voix","Milan",59000,"Italie","adresse le nom d'adresse"),
+("05 ","rue ","complement de voix","Lille",59000,"France","adressse le nom d'adresse"),
+("08 ","impasse "," voix","Londre",44000,"UK","ADRESSE angleterre, le nom d'adresse"),
+("03 ","rue ","complement de voix","Madrid",569000,"Espagne","adresse le nom d'adresse"),
+("06 ","avenue","complement de voix","Milan",52000,"Italie","adresse  le nom d'adresse"),
+("08 ","impasse "," voix","Londre",44000,"UK","ADRESSE angleterre, le nom d'adresse"),
+("07 ","rue ","complement de voix","Milan",59000,"Italie","adresse le nom d'adresse"),
+("03 ","rue ","complement de voix","Madrid",569000,"Espagne","adresse le nom d'adresse"),
+("08 ","allee ","complement de voix","Milan",59000,"France","le nom d'adresse"),
+("09 ","rue ","complement de voix","Lille",59000,"France","le nom d'adresse"),
+("03 ","rue ","complement de voix","Madrid",569000,"Espagne","adresse le nom d'adresse"),
+("010 ","allee ","complement de voix","Milan",59000,"Italie","le nom d'adresse"),
+("08 ","impasse "," voix","Londre",44000,"UK","ADRESSE angleterre, le nom d'adresse"),
+("011 ","rue ","complement de voix","Lille",59000,"France","le nom d'adresse")
+;
+
+/*les artistes*/
+insert into  artiste (biographie,nationalite,nom,prenom) 
 values 
-    ("01 ","rue ","complement de voix","Lille",59000,"France","le nom d'adresse");
+("Wilhelm von Abbema est né le 15 janvier 1812 à Krefeld1.
+ 18 ans, il s'inscrit à l'académie à Düsseldorf en tant qu'élève de Johann Wilhelm SchirmerCamille Bernier naît àvolution, le Consulat, l'Empire et le règne de Louis XVIII2.commencées sur le motif en Bretagne.générosité envers ses amis peintres qui trouvent gîte, couvert et parfois secours, tant à Paris qu'en Bretagne. Là aussi, il multiplie les dons et fonde en 1880 une école de jeunes filles à Saint-Méen dirigée par la congrégation des filles du Saint-Esprit, en souvenir de son épouse Lucie qui y possédait des terres.","allmende","Wilhelm","von Abbema"),
+("Ses parents sont des immigrés néerlandais. Il termie d'art, de 
+musique et de ballet de Pretoria en obtenant une distinction en art graphique et a
+meilleur peintre,Paul Ahyi naît en 1930 au Togo1.À partir de 1946, il fait ses études secondaires à Dakar, où il travaille également dans un cabinet d'architecture2. Il se rend en France et obtient connu pour ses œuvres d'art en plein air, ses reliefs et ses sculptures, dont ses contributions au monument de l'indépendance à Lomé, commémorant l'indépendance du Togo. On trouve d’autres sculptures et statues de lui sur des bâtiments et des parcs dans l’ensemble du Togo, ainsi qu’au Vatican, au Sénégal, au Bénin, en Côte d’Ivoire, au Nigéria et en Corée du Sud6. Il crée également ses pièces en utilisant un large éventail de médiums et matériaux, notamment des bijoux, des poteries, des céramiques et des tapisseries. Il est également designer 
+Il meurt dans la nuit du 3 au 4 janvier 2010 à son domicile de Lomé7.", "Togolaise", "Karl "," Abt"),
+("Il est connu pour ses œuvres d'art en plein air, ses reliefs et ses sculptu
+res, dont ses contributions au monument de l'indépendance à Lomé, commémorant l'ind
+épendance du Togo. On trouve d’autres
+ sculptures et statues de lui sur des bâtiments et des parcs dans l’ensemble
+ du Togo, ainsi qu’au Vatican, aures, dont ses contributions au monument de l'indépendance à Lomé, commémorant l'ind
+ sculptures et statues de lui sur des bâtiments et des parcs dans l’ensemble
+ du Togo, ainsi qu’au Vatican, au", "Togolaise", " Kuijers","David "),
+("Pierre-Jean Abatucci, né à Molenbeek-Saint-Jean le 20 mai 1871 et décédé à Ixelles le 20 décembre 1942, était un artiste peintre néo-impressionniste, aquafortiste et lithographe belge de paysages proche du symbolisme.Pierre Abatucci est né le 20 mai 1871 à Molenbeek-Saint-Jean1. Il est le fils de Jacques Abatucci et de Pétronille Senders. Il se maria à Catherine Louise de Mesmaeker.
+Il fit ses études artistiques à l'École des Arts Décoratifs de Molenbeek-Saint-Jean et ensuite de 1892 à 1897 à l'Académie royale des Beaux-Arts de  rue de la Loi, en 1921 et en 1935. En 1912 il y exposait déjà en collaboration avec Emile Jacques. En 1924 il y exposa conjointement avec Eric Wansart, Pros De Wit, Jenny Montigny et Henriëte Bossché. En 1920 il exposait au Salon d'automne avec notamment Firmin Baes, Henri Binard, Louis Buisseret, Hubert Glansdorff et Gustave Max Stevens.", "Belge", "Pierre  ","Abattucci "),
+("Ses parents sont des immigrés néerlandais. Il termina sa scolarité à l'École d'art, de musique et de ballet de Pretoria en obtenant une distinction en art graphique et a reçu, en 1980, le prix du meilleur peintre[réf. nécessaire].
+Après une brève période comme peintre d'enseignes, il obtint un diplôme de design graphique au Technikon du Cap, se classant premier en illustration. Diplômé en 1989, il reçut le prix d'honneur du designer/illustrateur. Il travailla brièvement en indépendant comme designer et illustrateur, mais 
+Il participe régulièrement à des expositions collectives et a honoré diverses commandes, notamment des marines acryliques pour le nouvel hôtel President et des aquarelles pour l'hôtel Villa Via et l'hôpital Volks.", "Anglaise", "Paul ","Ahyi"),
+("Sonia Abeloos, née à Bruxelles en 1876 et morte à Bruxelles en 1969, e
+st une artiste peintre belge qui a peint principalement des paysages, des marines et desst une artiste peintre belge qui a peint principalement des
+ paysages, des marines et desportraits.", "Belge", "Sonia  ","Abeloos "),
+("Abeloos, née à Bruxelles en 1876 et morte à Bruxelles en 1969 Il a beaucoup peint des tableaux de chevaux, de portraits équestres et des scènes de chasse.Emil Adam est le fils de peintre animalier Benno Adam et l'élève de son oncle Franz Adam et plus tard de Jean-François Portaels à Bruxelles. Il a épousé Joséphine Marie, née Wurmb et est le père du peintre Richard Benno Adam et du prêtre Ernst Adam.", "Française", "Tomma ","Abts"),
+("Henri Alphonse Barnoin, né Henri Alphonse Maximin Barnouin à Paris 6e le 7 juillet 18821, et mort à Clichy le 17 mars 19402, est un peintre français.Né dans un milieu d'artistes, Henri Barnoin est l'élève de Luc-Olivier Merson et de Émile Charles Dameron à l'École des beaux-arts de Paris.
+Au cours de l'année 1909, Henri Alphonse Barnoin expose pour la première fois ses œuvres au Salon de Paris et y obtient une mention honorable. Cela sera suivi par une médaille d'argent en 1921 et une médaille d'or en 1935.
+Il vient dès 1912 à Concarneau où il va s'installer à demeure une partie de l'année à partir de 1919. Ayant boutique sur le quai Pénéroff, il est le témoin privilégié de l'animation du port, qu'il ne se lasse pas de peindre et fait alors partie du groupe de Concarneau. On dénomme ainsi les eprésente aussi souvent des groupes de bretonnes en tenue traditionnelle, des pêcheurs et autres thèmes portuaires, des scènes de marché, des 
+Une centaine d’œuvres de ce peintre épris de couleur et de lumière ont été présentées au musée du Faouët lors d'une exposition qui lui a été consacrée en 20066.", "France", "Henri","Barnoin"),
+("Sonia Abeloos, née à Bruxelles en 1876 et morte à Bruxelles en 1969, est une artiste peintre belge qui a peint principalement des paysages, des marines et des portraits.Peintre de natures mortes, de marines et de portraits, Sonia Abeloos fut élève de l'Académie de Bruxelles, où elle étudia sous la direction dDillens et Rousseau.
+Elle a été membre du cercle artistique de Bruxelles.n 1966, à l'occasion de son 90e anniversaire, le Conseil national des femmes, dont elle était alors la doyenne d'âge, a voulu honorer l'artiste en organisant une visite de son atelier. Y étaient exposées des œuvres représentatives de ses 70 années d'activité.","Belge", "Sonnia "," Abelos"),
+("Émile Bernard, né le 28 avril 1868 à Lille et mort le 16 avril 1941 à Paris, est un peintre, graveur et écrivain français. Artiste postimpressionniste, il est associé à l'école de Pont-Aven. Il a fréquenté Vincent van Gogh, Paul Gauguin, Eugène Boch et plus tard Paul Cézanne.
+Ses œuvres les plus radicales ont été réalisées durant sa jeunesse, dans les années 1887-1892 où il participe aux innovations stylistiques de la fin du xixe siècle : il inaugure le cloisonnisme avec Louis Anquetin et Paul Gauguin. Ses recherches de simplification de la forme le conduisent vers le 
+Son travail littéraire est moins connu, il a écrit de la poésie sous le pseudonyme de Jean Dorsal, et également de la critique d'art.", "Française", "Emile"," Bernard"),
+("Eugène-Louis Boudin est un peintre françaijuillet 1824, mort à Deût 1898. Il fut l'un des premiers peintres français à saisir les paysages à l'extérieur d'un atelier. Grand peintre de marines, il est considéré comme l'un des précurseurs de l'impressionnisme.","Française","Eugène"," Boudin"),
+("Étienne Buffet, né à Paris le 5 juin 1866 et mort à Paris le 3 décembre 1948, est un artiste peintre français, de portraits, paysages et natures mortes.","Française","Étienne ","Buffet"),
+("Entré à l'École navale en 1954, tout en suivant les cours de École des beaux-arts de Paris, François Bellec poursuit une carrière atypique d'officier de marine occupant des affectations sur mer et sur terre, de peintre à travers les mers jusqu'au décret du président de la République du 7 décembre 1979 qui le fixe à Paris, détaché pour y entreprendre une nouvelle carrière de conservateur, comme directeur du musée national de la Marine, et développe ses établissements bretons du château de Brest et de la citadelle de Port-Louis entre 1980 et 1997. C'est également en 1980, qu'il devient président de l'Association des Peintres Officiels de la Marine.","Française","François "," Bellec"),
+("Adolphe Beaufrère, né le 24 mars 1876 à Quimprt le 16 février 1960 à Larmor-eintre et graveur français. Une grande partie de son œuvre est inspirée par la Bretagne.","Française","Adolphe "," Beaufrère"),
+("Ronan Barrot, né le 13 février 1973 à Carpentras1 dans le Vaucluse, est un peintre français.
+Il vit et travaille à Paris.","Française","Ronan "," Barrot"),
+("Robert Bluteare 1914 à Clichy et mort à Loctudy en février peintre et dessinateur français, également enseignant et proviseur.Issu d'une famille originaire de Vendée mais installée en région parisienne, Robert Bluteau se passionne pour les sciences, et, après des études à l'Ecole Nationale Supérieure Technique dedevient professeur de physique-chimie, puis directeur d'établissement technique. Il enseigne tout d'abord au lycée Kérichen de Brest, puis est nommé proviseur du lycée Jean-Charles Cazin à Boulogne-sur-Mer, un des plus anciens lycées","Française","Robert Bluteau"," Bluteau"),
+("Alfred Guillou reçoit ses premières leçons de dessin du lithographe Théodore Lemonnier qu'il rencontre à Concarneau en 1860. Sur ses conseils il s'installe à Paris en 1862, où il fréquente quelque temps l'Académie Suisse et poursuit sa formation avec Alexandre Cabanel. C'est là qu'il fait la connaissance de Jules Bastien-Lepage, Fernand Cormon et Théophile Deyrolle.
+Il débute au Salon de 1868 avec son Jeune Pêcheur breton, puis retourne dans sa ville natale. D'inspiration naturaliste, ses sujets de prédilection sont empruntés à la vie quotidienne du port de pêche souvent traités sur de grands formats, comme Les Sardinières de Concamusée des Beaux-Arts de Quiou encore le Débarquement du thon à Conca d'Art et d'Histoire de Sa.","Française","Alfred "," Guillou")
+;
