7500463eb931f2efe651d6b9e3318476edeb866f
Baziz <amir@baziz.fr>
2020-09-17 10:40:15 +0200

    Start java static function SQL

--- src/main/java/com/formation/kartina_spring/KartinaSpringApplication.java
+++ src/main/java/com/formation/kartina_spring/KartinaSpringApplication.java
@@ -1,5 +1,19 @@
 package com.formation.kartina_spring;
 
+import com.formation.kartina_spring.models.Adresse;
+import com.formation.kartina_spring.models.Article;
+import com.formation.kartina_spring.models.Artiste;
+import com.formation.kartina_spring.models.Cadre;
+import com.formation.kartina_spring.models.CodePromotionnel;
+import com.formation.kartina_spring.models.Commande;
+import com.formation.kartina_spring.models.Facture;
+import com.formation.kartina_spring.models.Finition;
+import com.formation.kartina_spring.models.Format;
+import com.formation.kartina_spring.models.Image;
+import com.formation.kartina_spring.models.Orientation;
+import com.formation.kartina_spring.models.Tag;
+import com.formation.kartina_spring.models.Theme;
+import com.formation.kartina_spring.models.Utilisateur;
 import com.formation.kartina_spring.services.ArticleService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,4 +29,32 @@ public class KartinaSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(KartinaSpringApplication.class, args);
     }
+
+
+   
+   
+   
+    public void saveDB () {
+    
+        Adresse adresse1= new Adresse();
+        Artiste artiste1 = new Artiste();
+        Article article1 = new Article();
+        Commande commande1 = new Commande();
+        Facture facture1 = new Facture();
+        Utilisateur utilsateur1 = new Utilisateur();
+
+        Cadre cadre1 = new Cadre();
+        Finition finition1= new Finition();
+        Format format1 = new Format();
+        Tag tag1 = new Tag();
+        Theme theme1 = new Theme();
+        Orientation orientation1 = new Orientation();
+        Image image1 = new Image();
+        CodePromotionnel codePromotionnel1 = new CodePromotionnel;
+
+
+
+    }
+
+
 }
