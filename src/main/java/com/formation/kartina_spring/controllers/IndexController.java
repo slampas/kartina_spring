a96e97a5288e147a326e4b1eacd32ecd0eea6b1b
Marc-Antoine DUPIRE <marc-antoinedupire@hotmail.fr>
2020-09-22 17:48:52 +0200

    modif css découpage de la page en fragment début implémentation controller

--- src/main/java/com/formation/kartina_spring/controllers/IndexController.java
+++ src/main/java/com/formation/kartina_spring/controllers/IndexController.java
@@ -3,6 +3,9 @@ package com.formation.kartina_spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class IndexController {
@@ -27,9 +30,48 @@ public class IndexController {
         return "index";
     }
 
+
     //Page parcours achat
     @GetMapping("/oeuvre")
     public String getOeuvre(Model model) {
+        //Rechercher BDD des format possible avec l'article selectionné, ref dans l'url ?
+        //...
+        model.addAttribute("fragmentForm", "format");
+        model.addAttribute("fragment", "parcours_achat");
+        return "index";
+    }
+
+    @PostMapping("/oeuvre")
+    public String postOeuvre(
+            Model model,
+            @RequestParam(name = "format") String format
+    ) {
+        //Sauvegarde du choix de format pour les autres controllers
+        //...
+        model.addAttribute("fragment", "parcours_achat");
+        return "index";
+    }
+
+    @GetMapping("/oeuvre/finition")
+    public String getOeuvreFinition(
+            Model model
+    ) {
+        //Rechercher BDD des finitions possible selon le format selectionné
+        //...
+
+        model.addAttribute("fragmentForm", "finition");
+        model.addAttribute("fragment", "parcours_achat");
+        return "index";
+        // "forward:index"
+    }
+
+    @PostMapping("/oeuvre/finition")
+    public String postOeuvreFinition(
+            Model model,
+            @RequestParam(name = "finition") String finition
+    ) {
+        //Sauvegarde du choix de la finition pour les autres controllers
+        //...
         model.addAttribute("fragment", "parcours_achat");
         return "index";
     }
--- src/main/resources/static/js/parcours_achat.js
+++ src/main/resources/static/js/parcours_achat.js
@@ -1,7 +1,7 @@
 function checkbox(element) {
+    debugger
     let form = element.form;
     let btn = form[form.length - 1];
-
     for (let i = 0; i < form.length - 1; i++) { // -1 pour le button
         //Un seul case coché à la fois possible
         form[i].checked = false;
@@ -10,5 +10,3 @@ function checkbox(element) {
     //Activation du bouton
     btn.disabled = false;
 }
-
-
